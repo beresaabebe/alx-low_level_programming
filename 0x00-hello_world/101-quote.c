538997e2e074412e9998e3bd971f1325f8e55e5b
beresaabebe <roobaanuuf@gmail.com>
2022-06-30 11:44:06 -0700

    Save changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main -everything starts here
- * return : return 1
+ * Return: always return failure
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
-	return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
