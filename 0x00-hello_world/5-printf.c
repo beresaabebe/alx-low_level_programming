4118c167802f5c70bbeba1e379c04c102365d6be
beresaabebe <roobaanuuf@gmail.com>
2022-06-30 10:28:47 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Everything starts here
+ * Return: always return 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
