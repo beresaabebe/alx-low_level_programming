cfa30e2c8aede87755fa9abd574e31ab606cdc09
beresaabebe <roobaanuuf@gmail.com>
2022-06-30 10:10:48 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
-int main(void)
+/**
+ * main - method
+ *
+ * always return 0 (success)
+ */
+
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
