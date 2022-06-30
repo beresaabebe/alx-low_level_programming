28f351f12ebbad0bf838da4f1bfd98b03d3972fb
beresaabebe <roobaanuuf@gmail.com>
2022-06-30 11:01:29 -0700

    8. Unix basically simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - is where everthing starts
+ * Return: this should return 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",61);
+       return (1);
+}       
