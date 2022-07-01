2cf76b7d713a7791dec935f501bc990bcd24bd66
beresaabebe <roobaanuuf@gmail.com>
2022-07-01 04:52:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - everything goes from here
+ * Return: always success comes here
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
