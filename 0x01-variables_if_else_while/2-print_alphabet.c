83b2dd3edfcd9499288f4641ce75f46ec2517d95
beresaabebe <roobaanuuf@gmail.com>
2022-07-01 04:20:29 -0700

    Alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- * main everything starts to run from here
  *
+ * main everything starts to run from here
  * Return: show success 0
+ *
  */
 int main(void)
 {
 	char c;
-	
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 
 	putchar('\n');
-	
 	return (0);
 }
