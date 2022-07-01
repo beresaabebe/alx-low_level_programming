b8804c81550da13c524702be26fc1dac90ef9ed4
beresaabebe <roobaanuuf@gmail.com>
2022-07-01 04:43:59 -0700

    Print all except q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - everything start from here
+ * Return: always return success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
