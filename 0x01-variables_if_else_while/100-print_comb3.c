5f27080238bc516889df818fc2109d24f2b9e0ee
beresaabebe <roobaanuuf@gmail.com>
2022-07-01 05:27:55 -0700

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Everything starts from here
+ * Return: always return success than failure
+ */
+int main(void)
+{
+	int c, n;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (n = '0'; n <= '9'; n++)
+		{
+			if (c < n)
+			{
+				putchar(c);
+				putchar(n);
+
+				if (c != '8' || (c== '8' && n != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
