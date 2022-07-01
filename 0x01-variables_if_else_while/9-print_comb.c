b9e3b910aa1a7d83784118ee3c58400ce358d9ce
beresaabebe <roobaanuuf@gmail.com>
2022-07-01 05:16:12 -0700

    Print all possible

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - everything goes from here
+ * Return: always return success than failure
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
