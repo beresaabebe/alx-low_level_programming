e50b61f6aa586b6461c18e6197b4111df44dfeea
beresaabebe <roobaanuuf@gmail.com>
2022-07-05 07:08:31 -0700

    Save changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,6 @@
-#include "main.h"
+q#include "main.h"
 
-/**
- * main - Everything goes from here
- * Return: always return sucess
- */
-int print_alphabet(void)
-{
-	int c;
+
 
 	for c = 'a'; c <= 'z'; c++)
 	{
