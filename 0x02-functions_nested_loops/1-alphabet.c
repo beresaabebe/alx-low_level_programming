7b7f0b0145c8b4326357977cab5f1d304e4be894
beresaabebe <roobaanuuf@gmail.com>
2022-07-05 06:52:38 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Everything goes from here
+ * Return: always return sucess
+ */
+int main(void)
+{
+	int c;
+
+	for c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
