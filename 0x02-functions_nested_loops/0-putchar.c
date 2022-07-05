ff198d0df2587fc8efefa46be70fe11f4613a3cd
beresaabebe <roobaanuuf@gmail.com>
2022-07-05 05:51:56 -0700

    Tasks 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - everything goes from here
+ * Return : always show as success value 
+ */
+
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
