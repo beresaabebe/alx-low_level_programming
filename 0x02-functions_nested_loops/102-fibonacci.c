f7b0ac21188f40abe331a0bc5b41132334d88e25
beresaabebe <roobaanuuf@gmail.com>
2022-07-06 03:51:28 -0700

    Save changes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,16 +5,19 @@
  */
 int main(void)
 {
-	int count, i = j =0, k;
+	unsigned long count, i, j, k;
+
+	i = 0;
+	j = 0;
 
 	for (count = 0; count < 50; count++)
 	{
 		k = i+j;
 		i = j;
 		j = k;
-		printf("%d", k);
+		printf("%lu", k);
 		if (count == 49)
-			printf('\n');
+			printf("\n");
 		else 
 			printf(",");
 	}
