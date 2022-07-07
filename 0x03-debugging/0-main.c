d8d9bfb90958eac7ac0aa55293fcc5b69aaf6d98
beresaabebe <roobaanuuf@gmail.com>
2022-07-06 21:02:31 -0700

    Multiple main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
  * main - everty thing goes from here
  * positive_or_negative - a funtion to check positive or negative or zero
- * @i - is value
+ * i: is int value
  * Return: return values
  */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
