99dfa7908ada51b1d6b649385af5dcc75f2dbde7
beresaabebe <roobaanuuf@gmail.com>
2022-07-08 05:50:51 -0700

    save

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints number 0 - 9
+ * Return: return success value
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '48'; c <='57'; c++)
+	{
+		putchar("%c", c);
+	}
+	putchar('\n');
+}
