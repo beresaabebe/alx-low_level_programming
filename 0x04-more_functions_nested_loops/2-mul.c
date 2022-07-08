ef078edbe799359760014c9b43b77f121a6c24ab
beresaabebe <roobaanuuf@gmail.com>
2022-07-08 05:32:59 -0700

    Save changes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - a function that return multiple of two number
+ * @a: first number
+ * @b: second number
+ * Return: return multple
+ */
+int mul(unsigned long a, unsigned long b)
+{
+  return a * b;
+}
