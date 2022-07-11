e3eb24ffb819bf100477cb93b6a6afd8b9202b92
beresaabebe <roobaanuuf@gmail.com>
2022-07-11 12:46:32 -0700

    Save

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
  * @dest: destination char values
  * @src : source pointer char values
  * Return : always return some values
- * return : values of dest 
+ * return : values of dest
  */
 char *_strcpy(char *dest, char *src)
 {
