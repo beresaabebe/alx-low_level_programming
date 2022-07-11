bb624b9c7d6866e2247d51ef90b18c9c2295b1cf
beresaabebe <roobaanuuf@gmail.com>
2022-07-11 13:03:58 -0700

    Save changes

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char password[22];
+	int i, n, pass. compl;
+
+	srand(time(0));
+
+	for (i = 0; pass < 2772; i++)
+	{
+		n = rand() % 10;
+		password[i] = seed[n];
+		pass += password[i];
+	}
+	compl = 2772 - pass;
+	password[i] = compl;
+	printf("%s\n", password);
+	return (0);
+}
