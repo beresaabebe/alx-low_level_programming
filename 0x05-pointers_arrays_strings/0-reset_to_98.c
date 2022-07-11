e43a6c7e6c0f7f21afe9cfe8c543805e0b031302
beresaabebe <roobaanuuf@gmail.com>
2022-07-11 11:06:31 -0700

    Save change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,10 @@
 #include "main.h"
-
+/**
+ * reset_to_98 - a function to check the code to reset
+ * @n: is a pointer int typoe
+ * Return: alway return some values
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
