f1fcbf848b5bf063ae2238dfef9dadcedfbf0657
beresaabebe <roobaanuuf@gmail.com>
2022-07-18 10:22:32 -0700

    Save

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - a function that set string
+ * @s: string to pass
+ * @to: Char to receive
+ * Return: nothing to return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
