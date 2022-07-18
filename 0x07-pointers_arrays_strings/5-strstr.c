e0350d23559863eb6775f8c096d8d94f7159bb10
Beresa Abebe <roobaanuuf@gmail.com>
2022-07-18 19:38:21 +0300

    save changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _strstr - locate a substring
- * @haystack: the to search
- * @needle: the to find 
- * Return: char value to be return
- */
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
 char *_strstr(char *haystack, char *needle)
 {
 	int a = 0, b = 0;
