03a8dc27573fd5c8f3960467428ca043472b202b
beresaabebe <roobaanuuf@gmail.com>
2022-07-20 10:51:58 -0700

    save

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,6 @@
  * sqrt_test -  a function makes possible to evaluate from 1 to n
  * @a: first number
  * @b: second number
- * 
- * _sqrt_recursion -  a function that return square root of number
- * @n: number
  * Return: on succes return some values
  */
 int sqrt_test(int a, int b)
@@ -14,8 +11,13 @@ int sqrt_test(int a, int b)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqrt_test(a, b + a));
+	return (sqrt_test(a, b + 1));
 }
+/**
+ * _sqrt_recursion - a function that return sqrt
+ * @n: number integer
+ * Return: return final result
+ */
 int _sqrt_recursion(int n)
 {
 	return (sqrt_test(n, 1));
