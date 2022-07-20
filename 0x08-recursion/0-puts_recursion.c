0440485a5669331f341314649939444e8a7952d5
beresaabebe <roobaanuuf@gmail.com>
2022-07-20 08:54:36 -0700

    save

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _put_recursion - a funtion that print a string folled by a new line
+ * @s: string
+ * Return: Return success value
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
