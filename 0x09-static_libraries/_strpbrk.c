70cf430b40c8f2270f415b99edeb0173f105728c
beresaabebe <roobaanuuf@gmail.com>
2022-07-23 01:18:55 -0700

    save again

---
+++ 0x09-static_libraries/_strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strpbrk - a function goes from here
+ * @s: first values
+ * @accept: second values
+ * Return: return some values
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/_strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - a function goes from here
+ * @haystack: first values
+ * @needle: second values
+ * Return: return some values
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
