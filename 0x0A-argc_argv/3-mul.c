73f292fcf9b344ee13f10b7fc6d3b5cb4aae1a0f
beresaabebe <roobaanuuf@gmail.com>
2022-07-24 01:53:02 -0700

    save

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function which start execution
+ * @argc: argument count
+ * @argv: argument vector
+ * @Return: return some values
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
