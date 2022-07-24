55c6b7e7d85b6ed6f508e505baa74caa4d88046d
beresaabebe <roobaanuuf@gmail.com>
2022-07-24 01:25:14 -0700

    save

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a function starts execution
+ * @argc: argument count
+ * @argv: array of argument vector
+ * Return: return some values
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
