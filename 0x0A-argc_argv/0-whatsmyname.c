c68902c2de5b0ec9ce155db430b99e928ee20ebf
beresaabebe <roobaanuuf@gmail.com>
2022-07-24 01:13:24 -0700

    save

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a function that start programming
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return some values
+ */
+int main(int argc, int *argv)
+{
+	return (0);
+}
